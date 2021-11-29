96fb8848f0696707a027cc9371f8ef6ec1ff127c
Raffaele Scarano <raffaelescarano28@gmail.com>
2021-11-29 19:36:28 +0100

    trying interface for bitmap

--- Coffeeffee/ImagePage.xaml.cs
+++ Coffeeffee/ImagePage.xaml.cs
@@ -4,13 +4,14 @@ using SkiaSharp;
 using Xamarin.Forms;
 using Coffeeffee.Models;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Coffeeffee
 {
     public partial class ImagePage : ContentPage
-        
+
     {
-       
+
         public ImagePage(Stream stream)
         {
             InitializeComponent();
@@ -23,6 +24,22 @@ namespace Coffeeffee
         async void Back_Clicked(System.Object sender, System.EventArgs e)
         {
             await Navigation.PopAsync();
+        }
+
+        private void ColorPicker_PickedColorChanged(object sender, Color colorPicked)
+        {
+            // do whatever you want with the colorPicked value
+        }
+
+        public Task<Stream> TransformPhotoAsync(Func<byte, byte, byte, double> pixelOperation)
+        {
+            return Task.Run(() =>
+            {
+                var bitmap = new Bitmap(image);
+                bitmap.ToPixelArray();
+                bitmap.TransformImage(pixelOperation);
+                return bitmap.ToImage().AsJPEG().AsStream();
+            });
         }
     }
 }
---
+++ Coffeeffee/Models/IBitmap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Coffeeffee.Models
+{
+    public interface IBitmap<T>
+    {
+        void ToPixelArray();
+
+        void TransformImage(Func<byte, byte, byte, double> pixelOperation);
+        T ToImage();
+    }
+}
