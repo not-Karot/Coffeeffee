1342e51224031372a2855ee6bb53ef6480889fe9
Raffaele Scarano <raffaelescarano28@gmail.com>
2021-12-03 15:23:21 +0100

    get most present color

--- Coffeeffee/ImagePage.xaml.cs
+++ Coffeeffee/ImagePage.xaml.cs
@@ -6,6 +6,8 @@ using Coffeeffee.Models;
 using System.IO;
 using System.Threading.Tasks;
 using Syncfusion.SfImageEditor.XForms;
+using System.Drawing;
+using Color = System.Drawing.Color;
 
 namespace Coffeeffee
 {
@@ -20,11 +22,7 @@ namespace Coffeeffee
            
             image.Source = ImageSource.FromStream(() => stream);
 
-            Console.WriteLine("constructor");
-
         }
-
-
 
         async void Home_Clicked(System.Object sender, System.EventArgs e)
         {
@@ -38,6 +36,41 @@ namespace Coffeeffee
             args.Cancel = true;
             var stream = args.Stream;
             
+            
+            SKBitmap bitmap =  SKBitmap.Decode(stream);
+
+            SKColor[] pixels = bitmap.Pixels;
+
+            bitmap.Pixels = pixels;
+            getMaxOccurrence(pixels);
+
+
+        }
+
+        public static void getMaxOccurrence(SKColor[] numbers)
+        {
+            var counts = new Dictionary<SKColor, int>();
+            foreach (SKColor number in numbers)
+            {
+                int count;
+                counts.TryGetValue(number, out count);
+                count++;
+                //Automatically replaces the entry if it exists;
+                //no need to use 'Contains'
+                counts[number] = count;
+            }
+            SKColor mostCommonNumber = 0;
+            int occurrences = 0;
+            foreach (var pair in counts)
+            {
+                if (pair.Value > occurrences)
+                {
+                    occurrences = pair.Value;
+                    mostCommonNumber = pair.Key;
+                }
+            }
+            Console.WriteLine("The most common number is {0} and it appears {1} times",
+                mostCommonNumber, occurrences);
         }
     }
 }
